aeba30d9ee635218ee81f78bf216ac0c4dd054d5
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-11 23:48:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,18 @@
 
 def reverse_each_word(str)
   array_of_characters = str.chars
-  reverse_array = []
+  reverse_word = []
+  reverse_sentence = []
   array_of_characters.each do |character|
+    if character == " "
+      #append the word to the sentence
+      reverse_sentence.push(reverse_word)
+    elsif condition
+
+    else
+
+    end
+
     reverse_array.unshift(character)
   end
   reverse_array
