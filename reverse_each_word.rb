532d23bc0c18d395d2108ad0943185f0e34ede34
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-11 23:44:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(str)
   array_of_characters = str.chars
   reverse_array = []
   array_of_characters.each do |character|
-    reverse_array.unshift(element)
+    reverse_array.unshift(character)
   end
   reverse_array
 end
