252e0ee7d901f364e3cc799c39a82dffe97a0f4b
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 00:05:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,45 +1,39 @@
 
 
 def reverse_each_word(str)
-  array_of_words = str.split(" ")
+  array_of_words = str.split(" ") #creates an array containing just the words as strings
+  p array_of_words
 
-
-
-
-
-
-
-
-
-
-
-
-  array_of_characters = str.chars
-  reverse_word = []
-  reverse_sentence = []
-  new_sentence = ""
-  array_of_characters.each do |character|
-    if character == " "
-      #append the word to the sentence
-      reverse_sentence.push(reverse_word)
-
-      reverse_word = []
-    else
-      if character == array_of_characters[-1] #if it's the last element
-        reverse_word.unshift(character)
-        reverse_sentence.push(reverse_word)
-      else
-      reverse_word.unshift(character)
-      end
-    end
-  end
-  reverse_sentence.each do |word|
-    new_sentence += word.join
-    if word != reverse_sentence[-1]
-      new_sentence += " "
-    end
-  end
-  new_sentence
 end
 
-p reverse_each_word("Hello World, I'm Haleigh")
+
+# array_of_characters = str.chars
+# reverse_word = []
+# reverse_sentence = []
+# new_sentence = ""
+# array_of_characters.each do |character|
+#   if character == " "
+#     #append the word to the sentence
+#     reverse_sentence.push(reverse_word)
+#
+#     reverse_word = []
+#   else
+#     if character == array_of_characters[-1] #if it's the last element
+#       reverse_word.unshift(character)
+#       reverse_sentence.push(reverse_word)
+#     else
+#     reverse_word.unshift(character)
+#     end
+#   end
+# end
+# reverse_sentence.each do |word|
+#   new_sentence += word.join
+#   if word != reverse_sentence[-1]
+#     new_sentence += " "
+#   end
+# end
+# new_sentence
+# end
+#
+# p reverse_each_word("Hello World, I'm Haleigh")
+# ###
