0dec44f01f339c6d1ed3b061cbf9f551411dec4e
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 00:21:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(str)
     array_of_chars = word.chars  #create an array of characters in the word
     #for every character in the word, add to the beginning of new word
     new_word = []
-    array_of_chars.collect { |character| new_word.unshift(character) }
+    array_of_chars.each { |character| new_word.unshift(character) }
     word = new_word.join
   end
   array_of_words.join(" ")
